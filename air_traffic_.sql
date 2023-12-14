cb048f106c0c04138f5421f1b7912ba91d6d55aa
Ahmed <a.hennawy97@hotmail.com>
2023-12-14 19:45:48 +0200

    Final commit

---
+++ air_traffic_.sql
@@ -0,0 +1,46 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "FLight" (
+    "id" INTEGER  PRIMARY KEY NOT NULL,
+    "seat" TEXT   NOT NULL,
+    "departure_date" DATE   NOT NULL,
+    "arrival_date" DATE   NOT NULL,
+    "passenger_id" INTEGER  REFERENCES Passengers(id) NOT NULL,
+    "airline_id" INTEGER  REFERENCES Airlines(id) NOT NULL,
+    "from_destination_id" INTEGER  REFERENCES Airports(id) NOT NULL,
+    "to_destination_id" INTEGER REFERENCES Airports(id)  NOT NULL,
+   
+);
+
+CREATE TABLE "Passengers" (
+    "id" INTEGER  PRIMARY KEY NOT NULL,
+    "first_name" TEXT   NOT NULL,
+    "last_name" TEXT   NOT NULL,
+    
+);
+
+CREATE TABLE "Airlines" (
+    "id" INTEGER   PRIMARY KEY NOT NULL,
+    "name" TEXT   NOT NULL,
+);
+
+CREATE TABLE "Airports" (
+    "id" INTEGER PRIMARY KEY NOT NULL,
+    "city" TEXT   NOT NULL,
+    "country" TEXT   NOT NULL,
+);
+
+ALTER TABLE "FLight" ADD CONSTRAINT "fk_FLight_passenger_id" FOREIGN KEY("passenger_id")
+REFERENCES "Passengers" ("id");
+
+ALTER TABLE "FLight" ADD CONSTRAINT "fk_FLight_airline_id" FOREIGN KEY("airline_id")
+REFERENCES "Airlines" ("id");
+
+ALTER TABLE "FLight" ADD CONSTRAINT "fk_FLight_from_destination_id" FOREIGN KEY("from_destination_id")
+REFERENCES "Airports" ("id");
+
+ALTER TABLE "FLight" ADD CONSTRAINT "fk_FLight_to_destination_id" FOREIGN KEY("to_destination_id")
+REFERENCES "Airports" ("id");
+
---
+++ outer-space.sql
@@ -0,0 +1,49 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Planets" (
+    "id" INTEGER   NOT NULL,
+    "name" TEXT   NOT NULL,
+    "orbital_period_in_years" INTEGER   NOT NULL,
+    "orbits_around_id" TEXT   NOT NULL,
+    "galaxy_id" TEXT   NOT NULL,
+    "moons_id" TEXT   NOT NULL,
+    CONSTRAINT "pk_Planets" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "Orbits_around" (
+    "id" INTEGER   NOT NULL,
+    "name" TEXT   NOT NULL,
+    CONSTRAINT "pk_Orbits_around" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "Moons" (
+    "id" INTEGER   NOT NULL,
+    "name" TEXT   NOT NULL,
+    CONSTRAINT "pk_Moons" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "Galaxy" (
+    "id" INTEGER   NOT NULL,
+    "name" TEXT   NOT NULL,
+    CONSTRAINT "pk_Galaxy" PRIMARY KEY (
+        "id"
+     )
+);
+
+ALTER TABLE "Planets" ADD CONSTRAINT "fk_Planets_orbits_around_id" FOREIGN KEY("orbits_around_id")
+REFERENCES "Orbits_around" ("id");
+
+ALTER TABLE "Planets" ADD CONSTRAINT "fk_Planets_galaxy_id" FOREIGN KEY("galaxy_id")
+REFERENCES "Galaxy" ("id");
+
+ALTER TABLE "Planets" ADD CONSTRAINT "fk_Planets_moons_id" FOREIGN KEY("moons_id")
+REFERENCES "Moons" ("id");
+
